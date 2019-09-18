b865099593b4c8a40b388f13f8a5debabb7be5f3
danielfarage <daniel.farage@yahoo.com.br>
2019-09-18 13:57:41 -0300

    DatePicker adicionado

--- app/src/main/java/com/example/teste_android/presentation/ui/InputDataActivity.kt
+++ app/src/main/java/com/example/teste_android/presentation/ui/InputDataActivity.kt
@@ -1,21 +1,22 @@
 package com.example.teste_android.presentation.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.text.Editable
-import android.text.SpannableStringBuilder
 import android.text.TextWatcher
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import com.example.teste_android.R
 import com.example.teste_android.presentation.common.toMoney
 import kotlinx.android.synthetic.main.activity_input_data.*
-import org.koin.core.KoinApplication.Companion.logger
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class InputDataActivity : AppCompatActivity() {
 
     private lateinit var moneyApplied: EditText
     private lateinit var endOfInvestiment: EditText
     private lateinit var cdiPercent: EditText
+    private val calendar = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,7 +44,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onTextChanged(newValue: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                if(editedText != newValue.toString()) {
+                if (editedText != newValue.toString()) {
                     moneyApplied.removeTextChangedListener(this)
 
                     val formatted = newValue
@@ -58,5 +59,26 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         })
+
+        endOfInvestiment.run {
+            isFocusable = false
+            setOnClickListener {
+                val year = calendar.get(Calendar.YEAR)
+                val month = calendar.get(Calendar.MONTH)
+                val day = calendar.get(Calendar.DAY_OF_MONTH)
+                DatePickerDialog(
+                    context,
+                    DatePickerDialog.OnDateSetListener { view, yearPicked, monthPicked, dayPicked ->
+
+                        endOfInvestiment.setText("$day/$monthPicked/$year")
+
+                    },
+                    year,
+                    month,
+                    day
+                ).show()
+            }
+        }
+
     }
 }
